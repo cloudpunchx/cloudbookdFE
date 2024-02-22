2d325694784906702e89691a04788e9b19dbd61e
cloudpunchx <siobhanbonardi@hotmail.com>
2024-02-21 22:37:33 -0700

    Made small change to v-footer, color to 'background' deep purple, added new styling and 2 links: github and portfolio.

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,12 +1,37 @@
 <template>
-    <v-footer class="gradient">
-        <v-col class="text-center">
-            <img
-                src="../assets/cloudpunchMediaBlk.png"
-                alt="Cloudpunch Media Logo"
-                width="150"
-            />
-        </v-col>
+    <v-footer color="background" elevation="24">
+        <v-container>
+            <v-row justify="center">
+                <v-col class="text-center" lg="1">
+                    <!-- Email Link -->
+                    <a href="mailto:siobhanbonardi@gmail.com" target="_blank">
+                        <v-icon color="white"> mdi-email </v-icon>
+                    </a>
+                </v-col>
+                <v-col class="text-center" lg="1">
+                    <!-- Cloudpunch Media Logo -->
+                    <a href="https://cloudpunch.ca/#/" target="_blank">
+                        <img
+                            src="../assets/cloudpunchMediaDark.png"
+                            alt="Cloudpunch Media Logo"
+                            width="150"
+                        />
+                    </a>
+                </v-col>
+                <v-col class="text-center" lg="1">
+                    <a
+                        href="https://github.com/cloudpunchx/cloudbookdFE"
+                        target="_blank"
+                    >
+                        <img
+                            src="../assets/github-mark-white.png"
+                            alt="Github Logo - white"
+                            width="25"
+                        />
+                    </a>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-footer>
 </template>
 
@@ -16,8 +41,4 @@
     };
 </script>
 
-<style scoped>
-    .gradient {
-        background: linear-gradient(to top, whitesmoke, #d0cadb);
-    }
-</style>
+<style scoped></style>
